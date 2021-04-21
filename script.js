59303992c981ed0ce4e787198109cc374898bd65
DvDRo5 <dromang5@gmail.com>
2021-04-21 09:04:35 +0200

    Add files

---
+++ script.js
@@ -0,0 +1,41 @@
+//Funcion para que aperezca a menos de 991px barras de navegación superiores ("la hamburguesa")
+
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+//función para cuando el usuario se pesplaza mas de 0 del viewport apareza el cohete para volver al inicio del HTML 
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+
+  // smooth scrolling o desplazamiento sueave  por el HTML
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
